f650734d2a98f34eedbd4668f7c20e56358c33ea
sy-orange <67741261+sy-orange@users.noreply.github.com>
2021-07-15 21:17:18 +0900

    initial comitt

---
+++ include/footer.js
@@ -0,0 +1,11 @@
+function footer(){
+    var html = "";
+    html += '<footer>';
+    html += '<div class="footer-content">';
+    html += '</div>'
+    html += '<div class="footer-logo">';
+    html += '<a href="/reguration-hp/index.html">CLUB WNR</a>';
+    html += '</div>';
+    html += '</footer>';
+    document.write(html);
+    }
---
+++ include/header.js
@@ -0,0 +1,25 @@
+
+function header(){
+    var html = "";
+    html +='<header>';
+    html +='<div class="header-content">';
+    html +='<div class="header-logo">';
+    html +='<a href="/reguration-hp/index.html">CLUB WNR</a>';
+    html +='</div>'
+    html +='<div class="header-nav">';
+    html +='<ul>';
+    html +='<li>';
+    html +='<a href="/reguration-hp/race_index.html">レース一覧</a>';
+    html +='</li>';
+    html +='<li>';
+    html +='<a href="/reguration-hp/room-rule.html">当部屋でのルール</a>';
+    html +='</li>';
+    html +='<li>';
+    html +='<a href="/reguration-hp/contact_us.html">お問い合わせ</a>';
+    html +='</li>';
+    html +='</ul>';
+    html +='</div>';
+    html +='</div>';
+    html +='</header>';
+    document.write(html);
+    }
